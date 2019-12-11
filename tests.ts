ffeadeded8a9e55accb7694c6887f2a00d91dce4
4tronix <Sales@4tronix.co.uk>
2019-12-11 15:56:24 +0000

    Added test file

--- tests.ts
+++ tests.ts
@@ -1,60 +1,49 @@
 {
-  // Drive full speed forward
-  MiniBit.drive(1023);
-  basic.pause(1000);
+// Set all 16 servos to the centre position
+ServoBit.centreServos();
+basic.pause(1000);
 
-  // Drive full speed reverse for 300 ms
-  MiniBit.driveMiliseconds(-1023, 300);
-  basic.pause(1000);
+// Set Servo 5 to +30 degrees
+ServoBit.setServo(5, 30);
+basic.pause(1000);
 
-  // Drive 100 ms forward on left and reverse on right
-  MiniBit.motor(MBMotor.Left, 1023);
-  MiniBit.motor(MBMotor.Right, -1023);
-  basic.pause(1000);
+// Set Servo 13 to -90 degrees
+ServoBit.setServo(13, -90);
+basic.pause(1000);
 
-  // Read sonar values
-  basic.showNumber(MiniBit.sonar(MBPingUnit.MicroSeconds));
-  basic.showNumber(MiniBit.sonar(MBPingUnit.Centimeters));
-  basic.showNumber(MiniBit.sonar(MBPingUnit.Inches));
+// Move servo 5 to 30 degrees at 40 degrees per second
+ServoBit.moveServo(5, 30, 40);
+basic.pause(3000);
 
-  // Set all leds to Red
-  MiniBit.setLedColor(MiniBit.MBColours(MBColors.Red));
-  MiniBit.ledShow();
-  basic.pause(1000);
+// Check current actual position of servo 5
+let actual = ServoBit.getServoActual(5);
+basic.pause(1000);
 
-  // Clear all leds
-  MiniBit.ledClear();
-  MiniBit.ledShow();
-  basic.pause(1000);
+// check current target positon for servo 5
+let target = ServoBit.getServoTarget(5);
+basic.pause(1000);
 
-  // Set led at position 2 to Blue
-  MiniBit.setPixelColor(2, MiniBit.MBColours(MBColors.Blue));
-  MiniBit.ledShow();
-  basic.pause(1000);
+// Wait for servo 5 to complete its movement
+ServoBit.waitServo(5);
+basic.pause(1000);
 
-  // Show led rainbow
-  MiniBit.ledRainbow();
-  MiniBit.ledShow();
-  basic.pause(1000);
+// Clear LED
+ServoBit.ledClear();
+basic.pause(1000);
 
-  // Show led rainbow and shift
-  MiniBit.ledRainbow();
-  MiniBit.ledShift();
-  MiniBit.ledShow();
-  basic.pause(1000);
+// Set LED to Red
+ServoBit.setLedColor(ServoBit.vColours(vColors.Red));
+basic.pause(1000);
 
-  // Show led rainbow and rotate
-  MiniBit.ledRainbow();
-  MiniBit.ledRotate();
-  MiniBit.ledShow();
-  basic.pause(1000);
+// Set brightness of LED
+ServoBit.ledBrightness(40);
+basic.pause(1000);
 
-  // Set brightness of leds
-  MiniBit.ledBrightness(100);
-  MiniBit.ledShow();
-  basic.pause(1000);
+// Start Flashing the LED with Green
+ServoBit.startFlash(ServoBit.vColours(vColors.Green), 100);
+basic.pause(3000);
 
-  // Set the Manual/Automatic Update mode
-  MiniBit.setUpdateMode(MBMode.Manual);
-  MiniBit.setUpdateMode(MBMode.Auto);
+// Stop flashing the LED
+ServoBit.stopFlash();
+basic.pause(1000);
 }
